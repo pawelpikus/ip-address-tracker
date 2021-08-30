bc7d0507684203557a069b92c064440f7101feb2
godspeed7 <pawelpikus7@gmail.com>
2021-08-30 17:05:44 +0200

    added map from leaflet.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
 import Card from "./Card"
+import 'leaflet/dist/leaflet.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCompass } from '@fortawesome/free-solid-svg-icons'
+import { faGreaterThan } from '@fortawesome/free-solid-svg-icons'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 
 function App() {
+  const position = [51.505, -0.09]
   return (
     <>  
       <div className="background-pattern"></div>
-      <div className="map"></div>
+      
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+             A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+      
       <div className="container">
         <header className="header">
           <h1 className="title">IP Address Tracker</h1>
@@ -20,7 +35,7 @@ function App() {
                 name="search-ip" 
                 id="search-ip"
                 placeholder="e.g. 192.168.1.106"/>
-              <button className="submit"><FontAwesomeIcon icon={faCompass} /></button>
+              <button className="submit"><FontAwesomeIcon icon={faGreaterThan} /></button>
             </form>
             
         </main>
